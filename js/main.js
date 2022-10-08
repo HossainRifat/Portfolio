7907ea70a7d12bae8b33155dafb72d28a2aefacc
HossainRifat <rh140025@gmail.com>
2022-10-08 12:24:44 +0600

    projects done

--- js/main.js
+++ js/main.js
@@ -5,6 +5,27 @@ window.addEventListener("scroll", function(){
 });
 
 //Portfolio section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
 
 //Our clients - Swiper
 
