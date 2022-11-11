2b2beb482cb110e652cea9e2fbf1c32baa8069f2
HossainRifat <rh140025@gmail.com>
2022-11-11 23:13:24 +0600

    responsive v1

--- js/main.js
+++ js/main.js
@@ -46,10 +46,55 @@ btn.addEventListener("click", () => {
 //Website dark/light theme
 
 //Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
 //Navigation menu items active on page scroll
+// window.addEventListener("scroll", () => {
+//     const sectionss = document.querySelectorAll("section");
+//     const scrollY = window.pageYOffset;
+ 
+//     sectionss.forEach(current => {
+//         let sectionHeight = current.offsetHeight;
+//         let sectionTop = current.offsetTop - 50;
+//         let id = current.getAttribute("id");
+
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+//         }
+//         else{
+//             document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+//         }
+//     });
+// });
 
 //Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
 
 //Scroll reveal animations
 //Common reveal options to create reveal animations
